030748520970d5ffc6badfe1c879d8200584e0c1
Bassent Gamal <bassent.gamal.88@gmail.com>
2025-06-10 11:50:13 +0300

    update openAI controller reading the key

--- UmbracoPOCV13/Controllers/OpenAiContentController.cs
+++ UmbracoPOCV13/Controllers/OpenAiContentController.cs
@@ -2,16 +2,24 @@ using Microsoft.AspNetCore.Mvc;
 using OpenAI_API;
 using System.Threading.Tasks;
 using Umbraco.Cms.Web.Common.Controllers;
+using Microsoft.Extensions.Configuration;
 
 namespace UmbracoPOCV13.Controllers
 {
     [Route("umbraco/api/[controller]/[action]")]
     public class OpenAiContentController : UmbracoApiController
     {
+        private readonly IConfiguration _configuration;
+        public OpenAiContentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpPost]
         public async Task<IActionResult> Generate([FromBody] PromptRequest request)
         {
-            var api = new OpenAIAPI("YOUR_OPENAI_API_KEY");
+            var apiKey = _configuration["OpenAI:ApiKey"];
+            var api = new OpenAIAPI(apiKey);
             var result = await api.Completions.CreateCompletionAsync(
                 new OpenAI_API.Completions.CompletionRequest(request.Prompt, model: "text-davinci-003", max_tokens: 100)
             );
